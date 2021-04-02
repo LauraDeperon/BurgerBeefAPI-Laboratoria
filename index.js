7f29e240c85b02d262dc3417e5f0adf68da2e6fc
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-04-02 17:43:31 -0300

    porta

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const routes = require('./server/routes/index');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV !== 'production'){
+  dotenv.config()
+}
 
 app.use(express.json());
 app.use('/', routes);
